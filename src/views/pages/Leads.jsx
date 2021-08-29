c289650229a9058ca75a41f02e02109c0ac5c689
Wesley Freitas de Lima <w.freitas.lima@gmail.com>
2021-08-28 22:20:06 -0300

    feat: add page leads

---
+++ src/views/pages/Leads.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function Leads() {
+  return (
+    <React.Fragment>
+      <Layout title="Painel de Leads">
+
+      </Layout>
+    </React.Fragment>
+  );
+}
