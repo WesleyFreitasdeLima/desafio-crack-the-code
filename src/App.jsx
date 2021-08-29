211b114baa1c82cb671a37f380d8b1dad982b85d
Wesley Freitas de Lima <w.freitas.lima@gmail.com>
2021-08-28 21:32:44 -0300

    feat: add rota para login

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./views/assets/styles/App.css";
 
+import Login from "./views/pages/Login";
 import Register from "./views/pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
   );
