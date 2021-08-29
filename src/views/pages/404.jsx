9acf470cb5580e3d11e978dd7d41d54367a2c35f
Wesley Freitas de Lima <w.freitas.lima@gmail.com>
2021-08-28 22:18:18 -0300

    feat: add pagina 404

---
+++ src/views/pages/404.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Page404() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Erro 404</h1>
+      <h3>Página não existe :(</h3>
+    </div>
+  );
+}
