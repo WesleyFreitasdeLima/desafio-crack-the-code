fb42c1d8b0d8d07e9d69a0cd066319b53a239448
Wesley Freitas de Lima <w.freitas.lima@gmail.com>
2021-08-28 22:21:54 -0300

    feat: add rotas privadas ao app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import "./views/assets/styles/App.css";
 
+import Page404 from "./views/pages/404";
 import Login from "./views/pages/Login";
 import Register from "./views/pages/Register";
+import Leads from "./views/pages/Leads";
+
+import { checkSession } from "./helpers/session";
+
+function PrivateRoutes() {
+  return (
+    <React.Fragment>
+      {checkSession() ? (
+        <Leads/>
+      ) : (
+        <Redirect to="/login" />
+      )}
+    </React.Fragment>
+  );
+}
 
 function App() {
   return (
@@ -11,6 +27,8 @@ function App() {
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        <PrivateRoutes path="/app" />
+        <Route path="*" component={Page404} />
       </Switch>
     </BrowserRouter>
   );
